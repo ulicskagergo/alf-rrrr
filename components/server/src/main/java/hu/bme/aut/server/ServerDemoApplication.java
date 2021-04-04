063f9bfdfa79be08ebd87c07b5f156c4f4934b6d
ulicskagergo <ulicskagergo@gmail.com>
2021-04-04 15:51:24 +0200

    Add connectivity for the driver component

--- components/server/src/main/java/hu/bme/aut/server/ServerDemoApplication.java
+++ components/server/src/main/java/hu/bme/aut/server/ServerDemoApplication.java
@@ -11,9 +11,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
+import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class ServerDemoApplication implements CommandLineRunner {
@@ -27,10 +29,34 @@ public class ServerDemoApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
         LightData lightData1 = saveAndFlushLightData("2021-03-14 20:09", true, 33, 30);
         LightData lightData2 = saveAndFlushLightData("2021-03-15 17:19", false, 0, 0);
         printLightDataBase(0, (int)serverRepository.count());
+
+        int BUFFER_LENGTH = 256;
+        String stringToSend = null, stringReceived = null;
+        System.out.println("Starting device test code example...");
+        System.out.println("Type in a short string to send to the kernel module:");
+        Scanner scanner = new Scanner(System.in);
+        stringToSend = scanner.toString();
+        System.out.println("Writing message to the device [" + stringToSend + "].");
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("/dev/ldrchar"))) {
+            bufferedWriter.write(stringToSend);
+        } catch (IOException ioException) {
+            System.err.println("Error while writing to file: " + ioException.getMessage());
+        }
+        System.out.println("Press ENTER to read back from the device...");
+        scanner.nextLine();
+        System.out.println("Reading from the device...");
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("/dev/ldchar"), BUFFER_LENGTH)) {
+            stringReceived = bufferedReader.readLine();
+        } catch (IOException ioException) {
+            System.err.println("Error while reading from file: " + ioException.getMessage());
+        }
+        System.out.println("The received message is: [" + stringReceived + "]");
+        System.out.println("End of the program");
     }
 
     public LightData saveAndFlushLightData(LightData lightData) {
