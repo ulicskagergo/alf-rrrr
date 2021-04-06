6a3866f104d3957a3e5e6029b526c233c17b80ad
ulicskagergo <ulicskagergo@gmail.com>
2021-04-06 17:28:48 +0200

    Implement data saving received from driver

--- components/server/src/main/java/hu/bme/aut/server/ServerDemoApplication.java
+++ components/server/src/main/java/hu/bme/aut/server/ServerDemoApplication.java
@@ -9,12 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 
 import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -29,53 +27,69 @@ public class ServerDemoApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        LightData lightData1 = saveAndFlushLightData("2021-03-14 20:09", true, 33, 30);
-        LightData lightData2 = saveAndFlushLightData("2021-03-15 17:19", false, 0, 0);
-        printLightDataBase(0, (int)serverRepository.count());
-
-        String stringToSend = null, stringReceived = null;
+        int dataToSend, dataReceived;
         System.out.println("Starting device test code example...");
-        System.out.println("Type in a short string to send to the kernel module:");
         Scanner scanner = new Scanner(System.in);
-        stringToSend = scanner.nextLine();
-        System.out.println("Writing message to the device [" + stringToSend + "].");
+        /*BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("/dev/ldrchar"));
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("/dev/ldchar"));*/
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("tmp.txt"));
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("tmp.txt"));
 
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("/dev/ldrchar"))) {
-            bufferedWriter.write(stringToSend);     // overwrite everything
+        while (true) {
+            LightData lightData = new LightData();
+
+            // read actual value
+            System.out.println("Type in a short string to send to the kernel module:");
+            dataReceived = readFromDevice(bufferedReader);
+            lightData.setActualValue((dataReceived != -1) ? dataReceived : 0);
+            lightData.setOn(dataReceived != -1);
+
+            // set threshold
+            dataToSend = scanner.nextInt();
+            if (dataToSend == -1) {
+                break;
+            }
+            writeToDevice(bufferedWriter, dataToSend);
+            lightData.setThreshold(dataToSend);
+
+            lightData.setMeasureDate(LocalDateTime.now());
+            lightData = saveAndFlushLightData(lightData);
+
+            printLightDataBase(0, (int)serverRepository.count());
+        }
+        bufferedReader.close();
+        bufferedWriter.close();
+        System.out.println("End of the program");
+    }
+
+    private void writeToDevice(BufferedWriter bufferedWriter, Integer dataToSend) {
+        try {
+            bufferedWriter.write(dataToSend);
+            bufferedWriter.flush();
         } catch (IOException ioException) {
             System.err.println("Error while writing to file: " + ioException.getMessage());
         }
-        System.out.println("Press ENTER to read back from the device...");
-        scanner.nextLine();
-        System.out.println("Reading from the device...");
+    }
 
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("/dev/ldchar"))) {
-            stringReceived = bufferedReader.readLine(); // read everything
+    private Integer readFromDevice(BufferedReader bufferedReader) {
+        Integer dataReceived = null;
+        try {
+            dataReceived = bufferedReader.read();
         } catch (IOException ioException) {
             System.err.println("Error while reading from file: " + ioException.getMessage());
         }
-        System.out.println("The received message is: [" + stringReceived + "]");
-        System.out.println("End of the program");
+        return dataReceived;
     }
 
     public LightData saveAndFlushLightData(LightData lightData) {
         return serverRepository.saveAndFlush(lightData);
     }
 
-    public LightData saveAndFlushLightData(LocalDateTime localDateTime, boolean isOn, Integer threshold, Integer actualValue) {
-        return serverRepository.saveAndFlush(new LightData(localDateTime, isOn, threshold, actualValue));
-    }
-
-    public LightData saveAndFlushLightData(String localDateTime, boolean isOn, Integer threshold, Integer actualValue) {
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-        return serverRepository.saveAndFlush(new LightData(LocalDateTime.parse(localDateTime, dateTimeFormatter), isOn, threshold, actualValue));
-    }
-
     public void printLightDataBase(int pageNum, int size) {
         Pageable pageable = PageRequest.of(pageNum, size);
         Page<LightData> page = serverRepository.findAll(pageable);
         page.forEach(System.out::println);
-        page.forEach(LightData::toJSON);
+        //page.forEach(LightData::toJSON);
     }
 
     // TODO print with sort and direction
