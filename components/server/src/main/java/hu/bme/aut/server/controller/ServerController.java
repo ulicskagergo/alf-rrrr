705edb00893cdcd21cfc2921ae776ff1d6e19590
ulicskagergo <ulicskagergo@gmail.com>
2021-05-03 21:22:17 +0200

    Add database dump and comment controller

--- components/server/src/main/java/hu/bme/aut/server/controller/ServerController.java
+++ components/server/src/main/java/hu/bme/aut/server/controller/ServerController.java
@@ -10,27 +10,83 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
+/**
+ * Controller for server application
+ */
 @RestController
 public class ServerController {
 
+    /**
+     * Repository for light data
+     */
     @Autowired
     private ServerRepository serverRepository;
 
+    /**
+     * Repository for measurement days
+     */
     @Autowired
     private MeasurementDayRepository measurementDayRepository;
 
+    /**
+     * Light model to take measurements
+     */
     @Autowired
     private LightModel lightModel;
 
-    // POST { "sensitivity": <0-100> sensorSensitivity, "from": "15:00", "to":"19:00" } /settings
+    /**
+     * Object for synchronize
+     */
+    private final Object monitor = new Object();
+
+    /**
+     * Jdbc template for database dump
+     */
+    private final JdbcTemplate jdbcTemplate;
+
+    /**
+     * Constructor to initialize jdbc template
+     *
+     * @param jdbcTemplate  give jdbc template
+     */
+    public ServerController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    /**
+     * Dump whole database into file with GET on /dumpdb
+     *
+     * @throws IOException  when there is a problem with dump file
+     */
+    @RequestMapping(value = "/dumpdb")
+    @ResponseBody
+    public void dumpdb() throws IOException {
+        synchronized (this.monitor) {
+            File file = new File("dump.sql");
+            if (file.exists()) {
+                file.delete();
+            }
+            this.jdbcTemplate.execute("script '" + file.getAbsolutePath() + "'");
+        }
+    }
+
+    /**
+     * Set sensitivity value and time range for measurements on /settings
+     * Pseudo: POST { "sensitivity": <0-100>, "from": "hh:mm", "to":"hh:mm" } /settings
+     *
+     * @param postBody  received POST message
+     */
     @RequestMapping(value = "/settings"
             , method = RequestMethod.POST,
             consumes = {MediaType.APPLICATION_JSON_VALUE})
@@ -43,7 +99,12 @@ public class ServerController {
         );
     }
 
-    // GET { "sensitivity": <0-100> sensorSensitivity, "from": "15:00", "to":"19:00" }  /settings
+    /**
+     * Get set sensitivity value and time range for measurements on /settings
+     * Pesudo: GET { "sensitivity": <0-100>, "from": "hh:mm", "to":"hh:mm" }  /settings
+     *
+     * @return  response entity with http status sign and data
+     */
     @RequestMapping(value = "/settings",
             produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<LightSettingsBody> getThreshold() {
@@ -51,13 +112,24 @@ public class ServerController {
         return new ResponseEntity<LightSettingsBody>(lightModel.exportSettings(), HttpStatus.OK);
     }
 
-    // GET days /dates
+    /**
+     * Get days on which measurement is taken on /dates
+     * Pseudo: GET ["2021-03-14","2021-03-15","2021-03-17"] /dates
+     *
+     * @return  response entity with http status sign and days
+     */
     @RequestMapping(value = "/dates")
     public ResponseEntity<List<LocalDate>> getDays() {
         return new ResponseEntity<>(measurementDayRepository.findMeasurementDates(), HttpStatus.OK);
     }
 
-    // GET all data per day /data/2021-05-30
+    /**
+     * Get measurement values on given day on /data/{specified_measure_day}
+     * Pseudo: GET [{"id":1001,"measureDate":"2021-03-14T15:43:11","isOn":true,"threshold":22,"actualValue":20},{..}] /data/2021-05-30
+     *
+     * @param measureDate
+     * @return
+     */
     @RequestMapping(value = "/data/{measure_date}")
     public ResponseEntity<List<LightData>> getDataByMeasureDate(@PathVariable("measure_date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate measureDate) {
         LocalDateTime measureDayTime = measureDate.atTime(0,0,0);
